2c597f071d2ab9c9fe9ed313a81bae9bd181a6d7
Joycevarg <joycev@cet.ac.in>
2019-12-06 23:49:18 +0530

    Page Indicator

--- lib/about.dart
+++ lib/about.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_page_indicator/flutter_page_indicator.dart';
 //Info("assets/images/bg2.jpg","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
 //Info("assets/images/bg2.jpg","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
 
@@ -6,18 +7,41 @@ class Aboutpage extends StatelessWidget
 {
   final controller=PageController(
   );
+  final pages=[
+  Info("assets/images/bg2.jpg","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."),
+  Info("assets/images/pg1.jpg","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."),
+    Info("assets/images/pg1.jpg","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
+
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(body:PageView(
-    controller: controller,
-      children: <Widget>[
-        Info("assets/images/bg2.jpg","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."),
-        Info("assets/images/pg1.jpg","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
+    return Scaffold(
 
-      ],
-    )
-    );
+        body:Column(children: <Widget>[
+      Container(
+          height: MediaQuery.of(context).size.height*0.95,
+          child:
+      PageView(
+    controller: controller,
+      children: pages,
+    )),
+      Container(
+        height: MediaQuery.of(context).size.height*0.05,
+        child:     PageIndicator(
+          layout: PageIndicatorLayout.SCALE,
+          size: 10.0,
+          controller: controller,
+          space: 5.0,
+          count: pages.length,
+          color: Colors.grey,
+          activeColor: Colors.black,
+        ),
+      )
+    ]
+    ));
   }
 }
 
@@ -38,9 +62,11 @@ class Info extends StatelessWidget
                 child:Image.asset(imgasst,height: MediaQuery.of(context).size.height*0.5,),
               )),
           Container(padding:EdgeInsets.only(left: 20,),child:Align(alignment:Alignment.centerLeft,child:Text("About",style: TextStyle(color:Colors.grey,fontSize: 30),textAlign: TextAlign.left,))),
-          Container(padding:EdgeInsets.fromLTRB(15, 5, 15, 10),child:Text(txt,style: TextStyle(color:Colors.black,fontSize: 20),textAlign: TextAlign.justify,))
+          Container(padding:EdgeInsets.fromLTRB(15, 5, 15, 10),child:Text(txt,style: TextStyle(color:Colors.black,fontSize: 20),textAlign: TextAlign.justify,)),
+
         ],
       ) ,
     );
   }
 }
+
